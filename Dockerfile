29e9e36ac0e7a6cc1b85cc1d24254ac8d2a3704a
Philippe Delebarre <pdelebarre@gmail.com>
2024-08-22 14:06:57 +0200

    adapting env files for local run

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM maven:3.8.5-openjdk-17
 # FROM maven:3.8.2-jdk-8 # for Java 8
 
-WORKDIR /bookappbackend
+WORKDIR /app
 COPY . .
 RUN mvn clean install
 
--- src/main/java/com/delebarre/bookappbackend/controller/BookController.java
+++ src/main/java/com/delebarre/bookappbackend/controller/BookController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
